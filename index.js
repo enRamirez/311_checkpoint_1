581120e4328837e5117e8182b1d53713902ee82a
enRamirez <ricky.ramirez125042@gmail.com>
2023-08-15 21:57:54 -0500

    initial files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const usersRouter = require('./routes/users')
+
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('default route'))
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
